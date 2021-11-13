ed9e46a84ed253514108fdbd2cf44bf11b441176
AdrianE-Gonzalez <adrian.e.gonzalez01@gmail.com>
2021-11-12 22:47:39 -0600

    Added Function To Load New Dataset

--- Code/Load_Dataset.py
+++ Code/Load_Dataset.py
@@ -6,7 +6,33 @@ from sklearn.preprocessing import LabelEncoder
 # Label Encoder Is Used To Convert Strings To Integers
 # EX. The String "QB" Is Set To Integer 15
 
+def dataset():
+    train_data = pd.read_csv("train.csv", sep=",")
 
+    # Initialize List To Store Columns Encoded
+    le_labels= []
+
+    # Loops Through Dataframe Columns And Apply Label Encoder Onto Any Columns With Strings Indexes
+    for z in train_data.columns:
+        if type(train_data.loc[0,z]) == str:
+            le = LabelEncoder()
+            train_data[z] = le.fit_transform(train_data[z])
+            le_labels.append(z)
+
+            # Saved Label Encoder For Later Use (To Decode Any Integer To It's Original String)
+            np.save('Dataset/Encoder/classes_'+str(z)+'.npy', le.classes_)
+
+        # print(str(z)+' == ' + str(dataset.loc[0,z]))
+    print('')
+
+    # for xs in le_labels:
+    #     encoder = LabelEncoder()
+    #     encoder.classes_ = np.load('classes_'+str(xs)+'.npy',allow_pickle=True)
+    #     dataset[xs] = encoder.inverse_transform(dataset[xs])
+    #     # print(str(xs)+' == ' + str(dataset.loc[0,xs]))    
+    
+    return train_data, le_labels
+    
 # Returns Dataset And List Of Columns Encoded
 def get_data():
     # Grab Dataset And Store It Onto A Pandas Dataframe
@@ -41,7 +67,7 @@ def get_data():
     
     return dataset, le_labels
 
-# get_data()
+dataset()
 
 
 
