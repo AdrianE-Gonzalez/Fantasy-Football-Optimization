7c86ae87a42b2c14fc4741b94ed55a5e0818d584
AdrianE-Gonzalez <adrian.e.gonzalez01@gmail.com>
2021-11-15 09:31:21 -0600

    Commented Out Line 5

--- Code/Main.py
+++ Code/Main.py
@@ -2,7 +2,7 @@ import Load_Dataset as ld
 import Linear_Regression as lr
 
 # Call Machine Learning Code
-dataset, le_labels= ld.get_data()
+# dataset, le_labels= ld.get_data()
 dataset, le_labels= ld.dataset()
 print('')
 lr.create_model(dataset,le_labels)
