91798c2ba23c7be6f26b299e2ea2e44389856cce
AdrianE-Gonzalez <adrian.e.gonzalez01@gmail.com>
2021-11-17 02:00:25 -0600

    Added Comments

--- Code/Linear_Regression.py
+++ Code/Linear_Regression.py
@@ -14,11 +14,11 @@ def create_model(train_dataset, test_dataset,test_le_labels):
     # Split X And Y Values
     # Y Is Set To The Fantasy Points Column
     # X Is Set To Everything Else
-    y_train= train['Actual']
-    X_train= train.drop('Actual',axis=1)
+    y_train= train_dataset['Actual']
+    X_train= train_dataset.drop('Actual',axis=1)
     
-    y_test= test['Actual']
-    X_test= test.drop('Actual',axis=1)
+    # y_test= test['Actual']
+    # X_test= test.drop('Actual',axis=1)
 
     # Create Model
     model = LinearRegression()
@@ -27,7 +27,6 @@ def create_model(train_dataset, test_dataset,test_le_labels):
 
     model.fit(X_train,y_train)
 
-    # predictions = model.predict(x_test)  # Gets a list of all predictions
     # Model Score
     r_sq = model.score(X_train, y_train)
     slope = model.coef_
@@ -36,6 +35,7 @@ def create_model(train_dataset, test_dataset,test_le_labels):
     print('coefficient of determination:', r_sq)
     print('intercept:', intercept)
     print('slope:', slope)
+
     # Predict
     y_pred = model.predict(x_test_data)
     print('predicted response:', y_pred, sep='\n')
@@ -46,16 +46,11 @@ def create_model(train_dataset, test_dataset,test_le_labels):
         encoder.classes_ = np.load('Dataset/Encoder/classes_test_'+str(xs)+'.npy',allow_pickle=True)
         test_dataset[xs] = encoder.inverse_transform(test_dataset[xs])
 
-    with open('Projected_Season.csv', mode='w') as flex:
+    # Used To Save Projected Fantasy Points
+    with open('Projected_Season_Linear_Regression.csv', mode='w') as flex:
         writer = csv.writer(flex, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL,lineterminator = '\n')
 
         for x in range(len(y_pred)):
             writer.writerow([test_dataset.iloc[x,0],y_pred[x]])
 
-    # # Used To Decode Label Encoder Based On Columns Encoded
-    # for xs in le_labels:
-    #     encoder = LabelEncoder()
-    #     encoder.classes_ = np.load('Dataset/Encoder/classes_'+str(xs)+'.npy',allow_pickle=True)
-    #     X_test[xs] = encoder.inverse_transform(X_test[xs])
-    #     print(X_test[xs])
     return r_sq, X_train,slope, intercept
