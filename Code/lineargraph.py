5f04b6cda914c0c7dcba2b40438c710a48714641
AdrianE-Gonzalez <adrian.e.gonzalez01@gmail.com>
2021-11-16 19:56:56 -0600

    Gets All Active Players From 2018 And Calculates The Actual Fantasy Points  Scored In 2018; Graphs The Comparison Between Projected vs Actual Fantasy Points

---
+++ Code/lineargraph.py
@@ -0,0 +1,90 @@
+import requests
+from bs4 import BeautifulSoup
+import re
+import csv
+import re
+import pandas as pd
+import matplotlib.pyplot as plt
+
+def run_graphs(r_sq,x,slope, intercept):
+    rootURL = 'https://www.pro-football-reference.com/'
+    playerURL_list = []
+    player_list = []
+
+    ppr = 0
+    ppf = 0.5
+
+    # HTML cleaner
+    def _strip_html(text):
+        tag_re = re.compile(r'<[^>]+>')
+        return tag_re.sub('', str(text))
+
+    url = 'https://www.pro-football-reference.com/years/2018/scrimmage.htm'
+    ## Scrape for RB stats
+    res = requests.get(url)
+    soup = BeautifulSoup(res.text,features="html.parser")
+
+    parsed = soup.findAll(
+        'table', {
+            'id': 'receiving_and_rushing'
+        }
+    )
+
+    rows = parsed[0].findAll('td')
+
+    column_len = 30
+
+    grouped_rows = [
+        rows[i:i+column_len] for i in range(0, len(rows), column_len)
+    ]
+
+    table = [map(lambda x: _strip_html(x), row) for row in grouped_rows]
+
+    for i in table:
+        player_list.append(list(i))
+
+    with open('Projected_Season.csv',mode='r') as flex:
+        data = []
+        reader = csv.reader(flex, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL,lineterminator = '\n')
+        with open('2018.csv',mode='w') as evaluate:
+            writer = csv.writer(evaluate,delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL,lineterminator = '\n')
+            for row in reader:
+                name = row[0]
+                proj = row[1]
+                for i in player_list:
+                    data = []
+                    if('*' in i[0]):
+                        i[0] = i[0][:-1]
+                    if(i[0] == name):
+                        data.append(name)
+                        data.append(proj)
+                        actual = int(i[27])/10 + int(i[28])*6 - 2*int(i[29]) + (ppr * int(i[7])) + ((int(i[11]) + int(i[20]))*ppf)
+                        difference = actual - float(proj)
+                        
+                        percent_difference = ((difference) / ((actual+float(proj))/2)) *100
+                        percent_difference = round(percent_difference, 2)
+                        #percent_difference = str(percent_difference)+'%'
+                        
+                        data.append(actual)
+                        data.append(difference)
+                        data.append(percent_difference)
+                        writer.writerow(data)
+                    else:
+                        continue
+
+    dataset = pd.read_csv("2018.csv", sep=",",names=['Names', 'Projected','Actual', 'Difference','Percent Difference'], header=None)
+
+    dataset= pd.DataFrame(dataset)
+    print(dataset)
+
+    y = intercept + slope * x
+
+    # Graphs
+    plt.scatter(dataset.iloc[:,2], dataset.iloc[:,1], color ='b')
+
+    for i,txt in enumerate(dataset.iloc[:,0]):
+        plt.annotate(txt, (dataset.iloc[:,2][i], dataset.iloc[:,1][i]))
+
+    plt.xlabel('Actual')
+    plt.ylabel('Projected')
+    plt.show()                    
