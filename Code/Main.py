921ca8c199bb61ada04ea38d5e80dfc7ef25198b
AdrianE-Gonzalez <adrian.e.gonzalez01@gmail.com>
2021-11-17 02:01:26 -0600

    Added LassoLarsCV Functions To Main.py

--- Code/Main.py
+++ Code/Main.py
@@ -1,8 +1,16 @@
 import Load_Dataset as ld
 import Linear_Regression as lr
+import lineargraph as lg
+import LassoLarsCV as lr2
 
 # Call Machine Learning Code
 # dataset, le_labels= ld.get_data()
-dataset, le_labels= ld.dataset()
+train_data, train_le_labels= ld.Train_Dataset()
+test_data, test_le_labels= ld.Test_Dataset()
 print('')
-lr.create_model(dataset,le_labels)
+
+# r_sq, x, slope, intercept= lr.create_model(train_data,test_data,test_le_labels)
+# lg.run_graphs(r_sq,x,slope,intercept,'Linear_Regression')
+
+r_sq, x, slope, intercept= lr2.create_model(train_data,test_data,test_le_labels)
+lg.run_graphs(r_sq,x,slope,intercept,'LassoLarsCV')
