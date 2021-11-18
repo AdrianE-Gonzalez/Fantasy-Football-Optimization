dc7a4a501c8ae0b009378031c38ad261ec5c39b8
AdrianE-Gonzalez <adrian.e.gonzalez01@gmail.com>
2021-11-18 03:28:53 -0600

    Added Boolean Parameters

--- Code/Main.py
+++ Code/Main.py
@@ -9,8 +9,8 @@ train_data, train_le_labels= ld.Train_Dataset()
 test_data, test_le_labels= ld.Test_Dataset()
 print('')
 
-# r_sq, x, slope, intercept= lr.create_model(train_data,test_data,test_le_labels)
-# lg.run_graphs(r_sq,x,slope,intercept,'Linear_Regression')
+r_sq, x, slope, intercept= lr.create_model(train_data,test_data,test_le_labels)
+lg.run_graphs(r_sq,x,slope,intercept,'Linear_Regression',True)
 
-r_sq, x, slope, intercept= lr2.create_model(train_data,test_data,test_le_labels)
-lg.run_graphs(r_sq,x,slope,intercept,'LassoLarsCV')
+# r_sq, x, slope, intercept= lr2.create_model(train_data,test_data,test_le_labels)
+# lg.run_graphs(r_sq,x,slope,intercept,'LassoLarsCV',True)
